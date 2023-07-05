0b0b3549aa4e7b67f4f3f985f8d1b76d1bcb3c9f
Oxidra <dantelezzo@gmail.com>
2023-07-05 13:22:40 +0200

    init

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php 
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>PHP Dischi json</title>
+
+        <link rel="stylesheet" href="./style/style.css">
+    </head>
+    <body>
+        <main>
+
+        </main>
+
+        <script src="https://unpkg.com/vue@3/dist/vue.global.js"></script>
+    </body>
+</html>
